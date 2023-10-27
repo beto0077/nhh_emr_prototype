1564a20ad4d0ccfd995760b5a61f5710057818c3
beto0077 <gilberto.gn.1997@gmail.com>
2023-10-27 11:43:17 -0600

    Cleaning old notes

--- server/server.js
+++ server/server.js
@@ -1,34 +1,3 @@
-/*import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import path from 'path';
-
-import Users from './routes/users.js';
-import Patient from './routes/users/Patient/patient.js';
-import Employee from './routes/users/Employee/employee.js';
-import Doctor from './routes/users/Doctor/doctor.js';
-import Admin from './routes/users/Administrator/admin.js';
-import SuperAdmin from './routes/users/Administrator/superAdmin.js';
-import Api from './routes/api/api.js';
-
-const app = express();
-
-app.use(cors());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/users', Users);
-app.use('/patient', Patient);
-app.use('/employee', Employee);
-app.use('/doctor', Doctor);
-app.use('/admin', Admin);
-app.use('/super', SuperAdmin);
-app.use('/api', Api);
-
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`));*/
-
 import express from 'express';
 import cors from 'cors';
 /*import {dirname, join} from 'path'
